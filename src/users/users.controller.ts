15fd85e745791df75ee3777fe173bcd57d49df7f
ShimJiHoon <wlgns2223@gmail.com>
2024-01-12 01:48:30 +0900

    feat: response객체에 status code 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,19 +3,20 @@ import {
   Controller,
   Delete,
   Get,
-  HttpException,
-  HttpStatus,
   Logger,
   Param,
   Post,
   Put,
   Query,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create.dto';
 import { UpdateUserDto } from './dto/update.dto';
 import { BaseResponse } from '../common/response/base';
 import { UserWithoutPassword } from './user.types';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -23,15 +24,16 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  async getUserByEmail(@Query('email') email: string) {
-    const res = new BaseResponse<UserWithoutPassword>();
+  async getUserByEmail(@Query('email') email: string, @Res() res: Response) {
+    const response = new BaseResponse<UserWithoutPassword>();
+
     try {
       const result = await this.usersService.getUserByEmail(email);
-      res.onSuccess(result);
+      response.onSuccess(result);
     } catch (error: any) {
-      res.onError(error.response.message, error.status);
+      response.onError(error.response.message, error.status);
     } finally {
-      return res.serialize();
+      return res.status(response.status).json(response.serialize());
     }
   }
 
